360cbb33b85e162fa82eb69f0dc6076639e7255f
Marc Grossi <tecnogrossi@gmail.com>
2024-06-13 17:38:57 +0200

    update angular version to 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
